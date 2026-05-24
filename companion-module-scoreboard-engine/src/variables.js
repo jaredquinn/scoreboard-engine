6b2dd3362dc212e3679cd4b6ea07d4df9de35804
Jared Quinn <jared@ssa.live>
2026-05-24 16:59:13 +1000

    Add support for Team Widgets to Companion

--- companion-module-scoreboard-engine/src/variables.js
+++ companion-module-scoreboard-engine/src/variables.js
@@ -3,8 +3,17 @@ module.exports = {
 		const defs = [];
 		for (const id of Object.keys(widgets)) {
 			defs.push({ variableId: id, name: id.replace(/_/g, ' ') });
+			if (widgets[id].type === 'List') {
+				defs.push({ variableId: `${id}_index`, name: `${id} Raw Index Value` });
+			}
+			if (widgets[id].type === 'Team') {
+				defs.push({ variableId: `${id}_name`, name: `${id} Full Name` });
+				defs.push({ variableId: `${id}_primary_color`, name: `${id} Primary Color` });
+				defs.push({ variableId: `${id}_secondary_color`, name: `${id} Secondary Color` });
+			}
 			if (widgets[id].type === 'Timer') {
-				defs.push({ variableId: `${id}_running`, name: `${id} Status` });
+				defs.push({ variableId: `${id}_running`, name: `${id} Status Boolean` });
+				defs.push({ variableId: `${id}_running_state`, name: `${id} Status Text` });
 				defs.push({ variableId: `${id}_raw`, name: `${id} Raw Value` });
 				defs.push({ variableId: `${id}_paused_formatted`, name: `${id} Formatted Stoppage Time` });
 				defs.push({ variableId: `${id}_paused_time`, name: `${id} Raw Stoppage Time` });
@@ -22,16 +31,23 @@ module.exports = {
 
 			if (type === 'Counter') {
 				values[id] = data.value;
+			} else if (type == 'Team') {
+				values[`${id}_name`] = data.name;
+				values[`${id}_primary_color`] = data.primary_color;
+				values[`${id}_secondary_color`] = data.secondary_color;
+				values[id] = data.short_name;
 			} else if (type === 'Timer') {
 				values[`${id}_paused_formatted`] = data.paused_formatted;
 				values[`${id}_paused_time`] = data.paused_time;
 				values[`${id}_paused`] = data.paused;
-				values[`${id}_running`] = data.running ? 'RUN' : 'STOP';
+				values[`${id}_running`] = data.running;
+				values[`${id}_running_state`] = data.running ? 'RUN' : 'STOP';
 				values[`${id}_raw`] = data.seconds;
 				values[id] = data.formatted_time;
 			} else if (type === 'List') {
 				const { index, options } = data;
 				values[id] = options[index] || '---';
+				values[`${id}_index`] = index;
 			} else if (type === 'Text') {
 				values[id] = data.content;
 			} else if (type === 'Calculation') {
